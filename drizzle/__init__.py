218889631077ff1518f0e2caa47ca316a70ddbcb
James Davies <jdavies@stsci.edu>
2019-03-12 22:52:44 -0400

    Update __init__.py

--- drizzle/__init__.py
+++ drizzle/__init__.py
@@ -1,14 +1,9 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
+"""
+A package for combining dithered images into a single image
+"""
+
 from __future__ import absolute_import, division, unicode_literals, print_function
 
-"""
-A package for combining dithered images into a single image 
-"""
-
-# Affiliated packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
 from ._astropy_init import *
-# ----------------------------------------------------------------------------
-
