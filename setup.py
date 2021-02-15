cc34ed865d4e0087fd6431ef1ce75584fee2ab4d
James Davies <jdavies@stsci.edu>
2021-02-15 14:29:53 -0500

    Clean up setup.py syntax

--- setup.py
+++ setup.py
@@ -9,29 +9,24 @@ import numpy
 
 
 def get_extensions():
-    ROOT = os.path.dirname(__file__)
-    SRCDIR = os.path.join(ROOT, 'src')
-    cfg = {
-        'include_dirs': [],
-        'libraries': [],
-        'define_macros': []
-    }
-
+    srcdir = os.path.join(os.path.dirname(__file__), 'src')
     cdriz_sources = ['cdrizzleapi.c',
                      'cdrizzleblot.c',
                      'cdrizzlebox.c',
                      'cdrizzlemap.c',
                      'cdrizzleutil.c',
                      os.path.join('tests', 'utest_cdrizzle.c')]
+    sources = [os.path.join(srcdir, x) for x in cdriz_sources]
 
-    sources = [os.path.join(SRCDIR, x) for x in cdriz_sources]
-
+    cfg = {
+        'include_dirs': [],
+        'libraries': [],
+        'define_macros': []
+    }
     cfg['include_dirs'].append(numpy.get_include())
-    cfg['include_dirs'].append(SRCDIR)
-
+    cfg['include_dirs'].append(srcdir)
     if sys.platform != 'win32':
         cfg['libraries'].append('m')
-
     if sys.platform == 'win32':
         cfg['define_macros'].append(('WIN32', None))
         cfg['define_macros'].append(('__STDC__', 1))
