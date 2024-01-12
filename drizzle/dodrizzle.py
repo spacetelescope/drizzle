ca40c1682b668cde51ff2db4eb2703f7f86bf8ec
Warren J. Hack <hack@stsci.edu>
2024-01-12 11:20:48 -0800

    Update new APIs

--- drizzle/dodrizzle.py
+++ drizzle/dodrizzle.py
@@ -15,11 +15,12 @@ def dodrizzle(insci, input_wcs, inwht,
               xmin=0, xmax=0, ymin=0, ymax=0,
               pixfrac=1.0, kernel='square', fillval="INDEF"):
     """
-    Low level routine for performing 'drizzle' operation.on one image.
+    Primary functional routine for performing 'drizzle' operation on one image.
 
     The interface is compatible with STScI code. All images are Python
     ndarrays, instead of filenames. File handling (input and output) is
-    performed by the calling routine.
+    performed by the calling routine.  WCS objects need to be based
+    on `astropy.wcs`.
 
     Parameters
     ----------
@@ -128,70 +129,35 @@ def dodrizzle(insci, input_wcs, inwht,
 
     """
 
-    # Ensure that the fillval parameter gets properly interpreted
-    # for use with tdriz
-    if util.is_blank(fillval):
-        fillval = 'INDEF'
-    else:
-        fillval = str(fillval)
-
-    if in_units == 'cps':
-        expscale = 1.0
-    else:
-        expscale = expin
-
-    # Add input weight image if it was not passed in
-
-    if (insci.dtype > np.float32):
-        insci = insci.astype(np.float32)
-
-    if inwht is None:
-        inwht = np.ones_like(insci)
-
-    # Compute what plane of the context image this input would
-    # correspond to:
-    planeid = int((uniqid - 1) / 32)
-
-    # Check if the context image has this many planes
-    if outcon.ndim == 3:
-        nplanes = outcon.shape[0]
-    elif outcon.ndim == 2:
-        nplanes = 1
-    else:
-        nplanes = 0
-
-    if nplanes <= planeid:
-        raise IndexError("Not enough planes in drizzle context image")
-
-    # Alias context image to the requested plane if 3d
-    if outcon.ndim == 3:
-        outcon = outcon[planeid]
-
+    # This could potentially be computed from the pixmap itself
+    # at the position of the input reference position.
     pix_ratio = output_wcs.pscale / wcslin_pscale
 
     # Compute the mapping between the input and output pixel coordinates
     pixmap = calc_pixmap.calc_pixmap(input_wcs, output_wcs)
 
     #
-    # Call 'drizzle' to perform image combination
-    # This call to 'cdriz.tdriz' uses the new C syntax
+    # Call `dodrizzle` to perform image combination
     #
-    _vers, nmiss, nskip = cdrizzle.tdriz(
-        insci, inwht, pixmap, outsci, outwht, outcon,
-        uniqid=uniqid, xmin=xmin, xmax=xmax,
-        ymin=ymin, ymax=ymax, scale=pix_ratio, pixfrac=pixfrac,
-        kernel=kernel, in_units=in_units, expscale=expscale,
-        wtscale=wt_scl, fillstr=fillval)
+    _vers, nmiss, nskip = apply_drizzle(insci, inwht, pixmap,
+                                        outsci, outwht, outcon,
+                                        expin, in_units, wt_scl,
+                                        pix_ratio=pix_ratio,
+                                        uniqid=uniqid,
+                                        xmin=xmin, xmax=xmax,
+                                        ymin=ymin, ymax=ymax,
+                                        pixfrac=pixfrac, kernel=kernel,
+                                        fillval=fillval)
 
     return _vers, nmiss, nskip
 
 
 def apply_drizzle(insci, inwht, pixmap,
-              outsci, outwht, outcon,
-              expin, in_units, wt_scl,
-              wcslin_pscale=1.0, uniqid=1,
-              xmin=0, xmax=0, ymin=0, ymax=0,
-              pixfrac=1.0, kernel='square', fillval="INDEF"):
+                  outsci, outwht, outcon,
+                  expin, in_units, wt_scl,
+                  pix_ratio=1.0, uniqid=1,
+                  xmin=0, xmax=0, ymin=0, ymax=0,
+                  pixfrac=1.0, kernel='square', fillval="INDEF"):
     """
     Low level routine for performing 'drizzle' operation.on one image.
 
@@ -241,8 +207,9 @@ def apply_drizzle(insci, inwht, pixmap,
     wt_scl : float
         A scaling factor applied to the pixel by pixel weighting.
 
-    wcslin_pscale : float, optional
-        The pixel scale of the input image. Conceptually, this is the
+    pix_ratio : float, optional
+        Ratio of output frame pixel scale to the
+        undistorted input pixel scale.  Conceptually, the input pixel scale is the
         linear dimension of a side of a pixel in the input image, but it
         is not limited to this and can be set to change how the drizzling
         algorithm operates.
@@ -339,7 +306,7 @@ def apply_drizzle(insci, inwht, pixmap,
 
     # Add input weight image if it was not passed in
 
-    if (insci.dtype > np.float32):
+    if insci.dtype > np.float32:
         insci = insci.astype(np.float32)
 
     if inwht is None:
@@ -363,9 +330,6 @@ def apply_drizzle(insci, inwht, pixmap,
     # Alias context image to the requested plane if 3d
     if outcon.ndim == 3:
         outcon = outcon[planeid]
-    # This could potentially be computed from the pixmap itself
-    # at the position of the input reference position.
-    pix_ratio = 1.0  # output_wcs.pscale / wcslin_pscale
 
     #
     # Call 'drizzle' to perform image combination
