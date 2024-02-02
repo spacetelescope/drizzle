132b21542e1c149a8d2156b94135b70eb747e6b8
Warren J. Hack <hack@stsci.edu>
2024-02-02 13:09:44 -0800

    Remove more doc changes

--- drizzle/calc_pixmap.py
+++ drizzle/calc_pixmap.py
@@ -12,7 +12,7 @@ def calc_pixmap(first_wcs, second_wcs):
         A WCS object representing the coordinate system you are
         converting from
 
-    second_wcs : wcs
+    seond_wcs : wcs
         A WCS object representing the coordinate system you are
         converting to
 
--- drizzle/dodrizzle.py
+++ drizzle/dodrizzle.py
@@ -15,12 +15,11 @@ def dodrizzle(insci, input_wcs, inwht,
               xmin=0, xmax=0, ymin=0, ymax=0,
               pixfrac=1.0, kernel='square', fillval="INDEF"):
     """
-    Primary functional routine for performing 'drizzle' operation on one image.
+    Low level routine for performing 'drizzle' operation.on one image.
 
     The interface is compatible with STScI code. All images are Python
     ndarrays, instead of filenames. File handling (input and output) is
-    performed by the calling routine.  WCS objects need to be based
-    on `astropy.wcs`.
+    performed by the calling routine.
 
     Parameters
     ----------
@@ -129,169 +128,6 @@ def dodrizzle(insci, input_wcs, inwht,
 
     """
 
-    # This could potentially be computed from the pixmap itself
-    # at the position of the input reference position.
-    pix_ratio = output_wcs.pscale / wcslin_pscale
-
-    # Compute the mapping between the input and output pixel coordinates
-    pixmap = calc_pixmap.calc_pixmap(input_wcs, output_wcs)
-
-    #
-    # Call `dodrizzle` to perform image combination
-    #
-    _vers, nmiss, nskip = apply_drizzle(insci, inwht, pixmap,
-                                        outsci, outwht, outcon,
-                                        expin, in_units, wt_scl,
-                                        pix_ratio=pix_ratio,
-                                        uniqid=uniqid,
-                                        xmin=xmin, xmax=xmax,
-                                        ymin=ymin, ymax=ymax,
-                                        pixfrac=pixfrac, kernel=kernel,
-                                        fillval=fillval)
-
-    return _vers, nmiss, nskip
-
-
-def apply_drizzle(insci, inwht, pixmap,
-                  outsci, outwht, outcon,
-                  expin, in_units, wt_scl,
-                  pix_ratio=1.0, uniqid=1,
-                  xmin=0, xmax=0, ymin=0, ymax=0,
-                  pixfrac=1.0, kernel='square', fillval="INDEF"):
-    """
-    Low level routine for performing 'drizzle' operation.on one image.
-
-    The interface is compatible with STScI code. All images are Python
-    ndarrays, instead of filenames. File handling (input and output) is
-    performed by the calling routine.
-
-    Parameters
-    ----------
-
-    insci : 2d array
-        A 2d numpy array containing the input image to be drizzled.
-        it is an error to not supply an image.
-
-    inwht : 2d array
-        A 2d numpy array containing the pixel by pixel weighting.
-        Must have the same dimensions as insci. If none is supplied,
-        the weghting is set to one.
-
-    pixmap : 3d array
-        Translation lookup table from input pixel position
-        to the output pixel frame.
-
-    outsci : 2d array
-        A 2d numpy array containing the output image produced by
-        drizzling. On the first call it should be set to zero.
-        Subsequent calls it will hold the intermediate results
-
-    outwht : 2d array
-        A 2d numpy array containing the output counts. On the first
-        call it should be set to zero. On subsequent calls it will
-        hold the intermediate results.
-
-    outcon : 2d or 3d array, optional
-        A 2d or 3d numpy array holding a bitmap of which image was an input
-        for each output pixel. Should be integer zero on first call.
-        Subsequent calls hold intermediate results.
-
-    expin : float
-        The exposure time of the input image, a positive number. The
-        exposure time is used to scale the image if the units are counts.
-
-    in_units : str
-        The units of the input image. The units can either be "counts"
-        or "cps" (counts per second.)
-
-    wt_scl : float
-        A scaling factor applied to the pixel by pixel weighting.
-
-    pix_ratio : float, optional
-        Ratio of output frame pixel scale to the
-        undistorted input pixel scale.  Conceptually, the input pixel scale is the
-        linear dimension of a side of a pixel in the input image, but it
-        is not limited to this and can be set to change how the drizzling
-        algorithm operates.
-
-    uniqid : int, optional
-        The id number of the input image. Should be one the first time
-        this function is called and incremented by one on each subsequent
-        call.
-
-    xmin : float, optional
-        This and the following three parameters set a bounding rectangle
-        on the input image. Only pixels on the input image inside this
-        rectangle will have their flux added to the output image. Xmin
-        sets the minimum value of the x dimension. The x dimension is the
-        dimension that varies quickest on the image. If the value is zero,
-        no minimum will be set in the x dimension. All four parameters are
-        zero based, counting starts at zero.
-
-    xmax : float, optional
-        Sets the maximum value of the x dimension on the bounding box
-        of the input image. If the value is zero, no maximum will
-        be set in the x dimension, the full x dimension of the output
-        image is the bounding box.
-
-    ymin : float, optional
-        Sets the minimum value in the y dimension on the bounding box. The
-        y dimension varies less rapidly than the x and represents the line
-        index on the input image. If the value is zero, no minimum  will be
-        set in the y dimension.
-
-    ymax : float, optional
-        Sets the maximum value in the y dimension. If the value is zero, no
-        maximum will be set in the y dimension,  the full x dimension
-        of the output image is the bounding box.
-
-    pixfrac : float, optional
-        The fraction of a pixel that the pixel flux is confined to. The
-        default value of 1 has the pixel flux evenly spread across the image.
-        A value of 0.5 confines it to half a pixel in the linear dimension,
-        so the flux is confined to a quarter of the pixel area when the square
-        kernel is used.
-
-    kernel: str, optional
-        The name of the kernel used to combine the input. The choice of
-        kernel controls the distribution of flux over the kernel. The kernel
-        names are: "square", "gaussian", "point", "tophat", "turbo", "lanczos2",
-        and "lanczos3". The square kernel is the default.
-
-    fillval: str, optional
-        The value a pixel is set to in the output if the input image does
-        not overlap it. The default value of INDEF does not set a value.
-
-    Returns
-    -------
-    A tuple with three values: a version string, the number of pixels
-    on the input image that do not overlap the output image, and the
-    number of complete lines on the input image that do not overlap the
-    output input image.
-
-
-    Notes
-    -----
-    Computation of the `pixmap` array prior to calling this function
-    requires the following steps:
-
-      1. Determine the size of the input image array
-      2. Create X and Y arrays where each element has a value
-         corresponding to it's X and Y position, respectively.
-      3. Convert those input X and Y input coordinates into
-         sky coordinates (RA, Dec) using the input image WCS.
-      4. Convert those sky coordinates into output pixel
-         coordinates using the output image WCS with all
-         X coordinates going to an X array, and Y coordinates
-         going to an Y array.
-      5. Write out output X,Y arrays as 3D array known as the pixmap.
-
-    The function `calc_pixmap()` in the module
-    `drizzle.drizzle.calc_pixmap` serves as an template for computing
-    the pixmap using astropy-compatible WCS inputs.
-
-    """
-
     # Ensure that the fillval parameter gets properly interpreted
     # for use with tdriz
     if util.is_blank(fillval):
@@ -306,7 +142,7 @@ def apply_drizzle(insci, inwht, pixmap,
 
     # Add input weight image if it was not passed in
 
-    if insci.dtype > np.float32:
+    if (insci.dtype > np.float32):
         insci = insci.astype(np.float32)
 
     if inwht is None:
@@ -330,6 +166,11 @@ def apply_drizzle(insci, inwht, pixmap,
     # Alias context image to the requested plane if 3d
     if outcon.ndim == 3:
         outcon = outcon[planeid]
+
+    pix_ratio = output_wcs.pscale / wcslin_pscale
+
+    # Compute the mapping between the input and output pixel coordinates
+    pixmap = calc_pixmap.calc_pixmap(input_wcs, output_wcs)
 
     #
     # Call 'drizzle' to perform image combination
