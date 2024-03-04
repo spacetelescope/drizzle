0889ef895052397875b4d9f7cdf03d3b81f470b9
Mihai Cara <mihail.cara@gmail.com>
2024-03-04 12:16:50 -0500

    fix ruff and formatting issues

--- drizzle/tests/test_utils.py
+++ drizzle/tests/test_utils.py
@@ -1,10 +1,10 @@
 import os
 
 import numpy as np
-from numpy.testing import assert_equal, assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_equal
+
 from astropy import wcs
 from astropy.io import fits
-
 from drizzle.utils import calc_pixmap, decode_context
 
 TEST_DIR = os.path.abspath(os.path.dirname(__file__))
